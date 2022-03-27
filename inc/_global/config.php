27c2fee488678904da03c0aee2d154be5946684c
hacker123456789123456789 <100495554+hacker123456789123456789@users.noreply.github.com>
2022-03-27 18:42:52 +0200

    Update config.php

--- inc/_global/config.php
+++ inc/_global/config.php
@@ -16,7 +16,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'yurni_gen');
 define('DB_USERNAME', 'yurni_gen');
 define('DB_PASSWORD', '123456789@azerty');
-define('ERROR_MESSAGE', 'Erreur dans la base de donnée !');	try {
+define('ERROR_MESSAGE', 'je ne trouve rien');	try {
 	$odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
 } catch( PDOException $Exception ) {
 	error_log('ERROR: '.$Exception->getMessage().' - '.$_SERVER['REQUEST_URI'].' at '.date('l jS \of F, Y, h:i:s A')."\n", 3, 'error.log');
